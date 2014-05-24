1ebfd467d4967d69d958ca3cae58b132a8948b04
Steve <steve.barnesjr@yahoo.com>
2014-05-24 13:41:07 -0700

    added user to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524170102) do
+ActiveRecord::Schema.define(version: 20140524202556) do
 
   create_table "messages", force: true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
